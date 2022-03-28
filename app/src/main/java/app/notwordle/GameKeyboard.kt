5ab94a67c72205f21b78a21233c0735ed7d5f2a8
s-merritt <smerritt987@gmail.com>
2022-03-28 17:35:09 -0500

    update keyboard to rough qwerty layout for letter keys

--- app/src/main/java/app/notwordle/GameKeyboard.kt
+++ app/src/main/java/app/notwordle/GameKeyboard.kt
@@ -17,56 +17,100 @@ class GameKeyboard @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) :
     LinearLayout(context, attrs, defStyleAttr), View.OnClickListener {
-    private var button1: Button? = null
-    private var button2: Button? = null
-    private var button3: Button? = null
-    private var button4: Button? = null
-    private var button5: Button? = null
-    private var button6: Button? = null
-    private var button7: Button? = null
-    private var button8: Button? = null
-    private var button9: Button? = null
-    private var button0: Button? = null
-    private var buttonDelete: Button? = null
-    private var buttonEnter: Button? = null
+
     private val keyValues = SparseArray<String>()
     private var inputConnection: InputConnection? = null
     private fun init(context: Context, attrs: AttributeSet?) {
         LayoutInflater.from(context).inflate(R.layout.keyboard, this, true)
-        button1 = findViewById<View>(R.id.button_1) as Button
-        button1!!.setOnClickListener(this)
-        button2 = findViewById<View>(R.id.button_2) as Button
-        button2!!.setOnClickListener(this)
-        button3 = findViewById<View>(R.id.button_3) as Button
-        button3!!.setOnClickListener(this)
-        button4 = findViewById<View>(R.id.button_4) as Button
-        button4!!.setOnClickListener(this)
-        button5 = findViewById<View>(R.id.button_5) as Button
-        button5!!.setOnClickListener(this)
-        button6 = findViewById<View>(R.id.button_6) as Button
-        button6!!.setOnClickListener(this)
-        button7 = findViewById<View>(R.id.button_7) as Button
-        button7!!.setOnClickListener(this)
-        button8 = findViewById<View>(R.id.button_8) as Button
-        button8!!.setOnClickListener(this)
-        button9 = findViewById<View>(R.id.button_9) as Button
-        button9!!.setOnClickListener(this)
-        button0 = findViewById<View>(R.id.button_0) as Button
-        button0!!.setOnClickListener(this)
-        buttonDelete = findViewById<View>(R.id.button_delete) as Button
+
+        // set up on click listeners
+        val buttonA = findViewById<Button>(R.id.button_a)
+        buttonA!!.setOnClickListener(this)
+        val buttonB = findViewById<Button>(R.id.button_b)
+        buttonB!!.setOnClickListener(this)
+        val buttonC = findViewById<Button>(R.id.button_c)
+        buttonC!!.setOnClickListener(this)
+        val buttonD = findViewById<Button>(R.id.button_d)
+        buttonD!!.setOnClickListener(this)
+        val buttonE = findViewById<Button>(R.id.button_e)
+        buttonE!!.setOnClickListener(this)
+        val buttonF = findViewById<Button>(R.id.button_f)
+        buttonF!!.setOnClickListener(this)
+        val buttonG = findViewById<Button>(R.id.button_g)
+        buttonG!!.setOnClickListener(this)
+        val buttonH = findViewById<Button>(R.id.button_h)
+        buttonH!!.setOnClickListener(this)
+        val buttonI = findViewById<Button>(R.id.button_i)
+        buttonI!!.setOnClickListener(this)
+        val buttonJ = findViewById<Button>(R.id.button_j)
+        buttonJ!!.setOnClickListener(this)
+        val buttonK = findViewById<Button>(R.id.button_k)
+        buttonK!!.setOnClickListener(this)
+        val buttonL = findViewById<Button>(R.id.button_l)
+        buttonL!!.setOnClickListener(this)
+        val buttonM = findViewById<Button>(R.id.button_m)
+        buttonM!!.setOnClickListener(this)
+        val buttonN = findViewById<Button>(R.id.button_n)
+        buttonN!!.setOnClickListener(this)
+        val buttonO = findViewById<Button>(R.id.button_o)
+        buttonO!!.setOnClickListener(this)
+        val buttonP = findViewById<Button>(R.id.button_p)
+        buttonP!!.setOnClickListener(this)
+        val buttonQ = findViewById<Button>(R.id.button_q)
+        buttonQ!!.setOnClickListener(this)
+        val buttonR = findViewById<Button>(R.id.button_r)
+        buttonR!!.setOnClickListener(this)
+        val buttonS = findViewById<Button>(R.id.button_s)
+        buttonS!!.setOnClickListener(this)
+        val buttonT = findViewById<Button>(R.id.button_t)
+        buttonT!!.setOnClickListener(this)
+        val buttonU = findViewById<Button>(R.id.button_u)
+        buttonU!!.setOnClickListener(this)
+        val buttonV = findViewById<Button>(R.id.button_v)
+        buttonV!!.setOnClickListener(this)
+        val buttonW = findViewById<Button>(R.id.button_w)
+        buttonW!!.setOnClickListener(this)
+        val buttonX = findViewById<Button>(R.id.button_x)
+        buttonX!!.setOnClickListener(this)
+        val buttonY = findViewById<Button>(R.id.button_y)
+        buttonY!!.setOnClickListener(this)
+        val buttonZ = findViewById<Button>(R.id.button_z)
+        buttonZ!!.setOnClickListener(this)
+
+        val buttonDelete = findViewById<Button>(R.id.button_delete)
         buttonDelete!!.setOnClickListener(this)
-        buttonEnter = findViewById<View>(R.id.button_enter) as Button
+        val buttonEnter = findViewById<Button>(R.id.button_enter)
         buttonEnter!!.setOnClickListener(this)
-        keyValues.put(R.id.button_1, "1")
-        keyValues.put(R.id.button_2, "2")
-        keyValues.put(R.id.button_3, "3")
-        keyValues.put(R.id.button_4, "4")
-        keyValues.put(R.id.button_5, "5")
-        keyValues.put(R.id.button_6, "6")
-        keyValues.put(R.id.button_7, "7")
-        keyValues.put(R.id.button_8, "8")
-        keyValues.put(R.id.button_9, "9")
-        keyValues.put(R.id.button_0, "0")
+
+        // map key values
+        keyValues.put(R.id.button_a, "a")
+        keyValues.put(R.id.button_b, "b")
+        keyValues.put(R.id.button_c, "c")
+        keyValues.put(R.id.button_d, "d")
+        keyValues.put(R.id.button_e, "e")
+        keyValues.put(R.id.button_f, "f")
+        keyValues.put(R.id.button_g, "g")
+        keyValues.put(R.id.button_h, "h")
+        keyValues.put(R.id.button_i, "i")
+        keyValues.put(R.id.button_j, "j")
+        keyValues.put(R.id.button_k, "k")
+        keyValues.put(R.id.button_l, "l")
+        keyValues.put(R.id.button_m, "m")
+        keyValues.put(R.id.button_n, "n")
+        keyValues.put(R.id.button_o, "o")
+        keyValues.put(R.id.button_p, "p")
+        keyValues.put(R.id.button_q, "q")
+        keyValues.put(R.id.button_r, "r")
+        keyValues.put(R.id.button_s, "s")
+        keyValues.put(R.id.button_t, "t")
+        keyValues.put(R.id.button_u, "u")
+        keyValues.put(R.id.button_v, "v")
+        keyValues.put(R.id.button_w, "w")
+        keyValues.put(R.id.button_x, "x")
+        keyValues.put(R.id.button_y, "y")
+        keyValues.put(R.id.button_z, "z")
+
+        // TODO this shouldn't new line but should try to check the grid
         keyValues.put(R.id.button_enter, "\n")
     }
 
